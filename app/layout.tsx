1e883c2a113e2da70acdedc86732226770b0128f
juanelonz <jm.godoy@outlook.com>
2026-06-11 09:36:42 -0300

    pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,9 +43,30 @@ export default function RootLayout({
             gtag('js', new Date());
             gtag('config', 'G-D7CDFL01Y9');`}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1690671288505049');
+            fbq('track', 'PageView');`}
+        </Script>
       </head>
       <body className="font-sans antialiased">
         {children}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1690671288505049&ev=PageView&noscript=1"
+            alt="Facebook Pixel"
+          />
+        </noscript>
         {process.env.NODE_ENV === 'production' && <Analytics />}
       </body>
     </html>
