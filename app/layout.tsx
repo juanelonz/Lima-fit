08ead532d8c13666c6cd5456343b99e81347a404
juanelonz <jm.godoy@outlook.com>
2026-06-11 13:19:05 -0300

    clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,6 +55,13 @@ export default function RootLayout({
             fbq('init', '1690671288505049');
             fbq('track', 'PageView');`}
         </Script>
+        <Script id="clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "x5g22atgne");`}
+        </Script>
       </head>
       <body className="font-sans antialiased">
         {children}
